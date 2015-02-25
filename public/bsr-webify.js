980a25b9d71496330e67149829b76f68165233f9
csmpls <yes@cosmopol.is>
2015-02-25 15:54:05 -0800

    added a dropdown where users can select which article they want

--- public/bsr-webify.js
+++ public/bsr-webify.js
@@ -1,15 +1,41 @@
 var socket = io.connect()
 
-socket.on('new_word', function (data) {
+socket.on('articles_list', function (data) {
+  populate_articles_dropdown(data)
+})
+
+socket.on('show_word', function (data) {
   set_word(data)
 })
 
-	$(window).on('beforeunload', function(){
-  	socket.close()
+socket.on('show_questions', function (data) {
+  $('#wordContainer').hide()
+  set_questions(data)
 })
+
+$(window).on('beforeunload', function(){
+	socket.close()
+})
+
+function populate_articles_dropdown(article_titles) {
+  var options = $("#options")
+  $.each(article_titles, function() {
+    options.append($("<option />").text(this));
+  })
+}
+
+function sendArticle() {
+	articleTitle = $("#options").children("option").filter(":selected").text()
+	socket.emit('article_selection', articleTitle)
+}
 
 function set_word(word) {
   $('#wordContainer').html(strip_quotes(word))
+}
+
+function set_questions(questions) {
+  $('#questionsContainer').html(questions)
+
 }
 
 function strip_quotes(word) {
--- webserver.js
+++ webserver.js
@@ -2,6 +2,8 @@ var express = require('express'),
 	app = express()
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var fs = require('fs')
+var _ = require('lodash')
 
 var bodyParser = require('body-parser')
 
@@ -9,6 +11,9 @@ app.use("/public", express.static(__dirname + '/public'))
 
 // parse application/json
 app.use(bodyParser.json())
+
+// an object with all the titles, filepaths, contents of the articles
+var articlesJson = listArticles()
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -31,6 +36,16 @@ app.post('/show_questions', function (req, res) {
 // connect to client socket
 io.on('connection', function (socket) {
   console.log('connected to client socket')
+  io.sockets.emit('articles_list', getArticleTitles()) 
+
+  socket.on('article_selection', function(data) {
+    article_title = data
+    article = _.filter(articlesJson, function(article) {
+      return article.title === article_title
+    })
+    console.log(article)  // todo: execute the python script with the article path as an argument
+  })
+
 });
 
 // listen
@@ -39,3 +54,24 @@ server.listen(3000, function () {
   var port = server.address().port
   console.log('Example app listening at http://%s:%s', host, port)
 })
+
+
+function getArticleTitles() {
+  return _.map(articlesJson, function(article) {
+    return article.title
+  })
+}
+
+// read the articles from the json file on disk
+function listArticles() {
+ return _.map(fs.readdirSync('articles/'), readArticleData)
+}
+
+function readArticleData(article) {
+  var path = 'articles/'+article
+  json = JSON.parse(
+    fs.readFileSync(path , {encoding: 'utf-8'})
+  )
+  json.path = path
+  return json
+}
